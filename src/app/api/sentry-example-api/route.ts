523fb60dcac5acfe3614e406a9ba59461beee451
Sam Bonin <samuel.o.bonin@gmail.com>
2025-12-30 00:18:37 -0700

    Revert "feat: add Sentry example page for testing error tracking"
    This reverts commit 05324bbd62b7774a6caf4ed40aabf62203179389.

--- src/app/api/sentry-example-api/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  throw new Error("Sentry API Test Error");
-  return NextResponse.json({ message: "This will never be reached" });
-}
